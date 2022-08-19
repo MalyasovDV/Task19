05248ffceb5e7e4d0ac24e2e0b9f2c2dc5cfd708
Dmitry_Malyasov <dmalyasov24@gmail.com>
2022-08-19 10:02:07 +0800

    Task done

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿void Palindrome(int number)
+{
+    if (number / 10000 == number % 10)
+    {
+        number %= 10000;
+        number /= 10;
+
+        if (number / 100 == number % 10)
+            Console.WriteLine("Yes");
+        else
+            Console.WriteLine("No");
+    }
+    else
+        Console.WriteLine("No");
+}
+
+int number = 14212;
+Palindrome(number);
+
+number = 12821;
+Palindrome(number);
+
+number = 23432;
+Palindrome(number);
